89b60e7ece26cbf30e01dbb1a1086a62f7fdbcf2
Logesh DJEARAM <logeshdjearam@MacBook-Pro-de-Logesh.local>
2024-11-20 22:06:23 +0100

    Test

--- src/containers/Test/test.js
+++ src/containers/Test/test.js
@@ -1,4 +1,4 @@
-const Sections = () => {
+const Test = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid gap-5 row-gap-10 lg:grid-cols-2">
@@ -151,4 +151,4 @@ const Sections = () => {
   );
 };
 
-export default Sections;
+export default Test;
